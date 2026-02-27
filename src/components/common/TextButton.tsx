f3f6599727f6481fece280bcb6018d50c42f5bca
jgarzon26 <janusseangarzon@gmail.com>
2026-02-27 15:14:38 +0800

    added hover for button

--- src/components/common/TextButton.tsx
+++ src/components/common/TextButton.tsx
@@ -11,7 +11,7 @@ const TextButton: FC<Props & PropsWithChildren> = ({ className, onClick, childre
     <button 
       type="button" 
       onClick={onClick} 
-      className={twMerge('bg-button-primary text-button-primary-foreground flex items-center justify-center px-15 py-5', className)}
+      className={twMerge('bg-button-primary text-button-primary-foreground flex items-center justify-center px-15 py-5 hover:cursor-pointer', className)}
     >
       {children}
     </button>
