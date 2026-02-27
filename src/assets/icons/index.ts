90651b33abb385b9dea6f5801b548f262c2335fe
jgarzon26 <janusseangarzon@gmail.com>
2026-02-27 15:44:52 +0800

    added icons for product card

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Compare } from './compare.png';
+export { default as Favorite } from './favorite.png';
+export { default as Logo } from './furniro-logo.png';
+export { default as Share } from './share.png';
