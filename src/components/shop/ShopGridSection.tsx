2f85840e90a4388a9b442d1f958b94f0fd8d20b3
jgarzon26 <janusseangarzon@gmail.com>
2026-03-03 14:24:09 +0800

    Update ShopGridSection.tsx

--- src/components/shop/ShopGridSection.tsx
+++ src/components/shop/ShopGridSection.tsx
@@ -3,7 +3,7 @@ import { CardItem } from "../common";
 const ShopGridSection = () => {
   return (
     <section className="p-20">
-      <ul className="grid grid-cols-5 gap-y-7">
+      <ul className="grid grid-cols-5 gap-7">
         {
           Array.from({length: 16}).map((_, index) => (
             <CardItem key={index}/>
