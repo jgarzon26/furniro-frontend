b7a7eccfc3572be3b2b6c7dc119e2505d757c33e
jgarzon26 <janusseangarzon@gmail.com>
2026-03-04 15:36:37 +0800

    using provider for shop list

--- src/components/shop/ShopGridSection.tsx
+++ src/components/shop/ShopGridSection.tsx
@@ -1,11 +1,16 @@
+'use client';
+
+import { useShopOptionsStore } from "@/hooks/useShopOptionsStore";
 import { CardItem } from "../common";
 
 const ShopGridSection = () => {
+  const { show } = useShopOptionsStore(state => state);
+
   return (
     <section className="p-20">
       <ul className="grid grid-cols-5 gap-7">
         {
-          Array.from({length: 16}).map((_, index) => (
+          Array.from({length: show}).map((_, index) => (
             <CardItem key={index}/>
           ))
         }
