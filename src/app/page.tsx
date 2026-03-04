101caa0b84e6c246f175b6789c56be40fbe12657
jgarzon26 <janusseangarzon@gmail.com>
2026-03-04 12:04:48 +0800

    route changing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-const RootPage = () => {
-  return <main></main>;
+import { CategorySection, IntroSection, ProductSection, DisplaySection, ImageGallerySection } from "@/components/home";
+
+const HomePage = () => {
+  return (
+    <main>
+      <IntroSection />
+      <CategorySection />
+      <ProductSection />
+      <DisplaySection />
+      <ImageGallerySection />
+    </main>
+  );
 }
 
-export default RootPage;
+export default HomePage;
