51aa6973845987c3282921b69486beef833fd01d
jgarzon26 <janusseangarzon@gmail.com>
2026-03-06 15:30:25 +0800

    setup structure

---
+++ src/app/cart/layout.tsx
@@ -0,0 +1,17 @@
+import { IntroSection } from "@/components/common";
+import Footer from "@/components/footer/Footer";
+import FooterBenefits from "@/components/footer/FooterBenefits";
+import { FC } from "react";
+
+const CartLayout: FC<LayoutProps<'/cart'>> = ({children}) => {
+  return (
+    <>
+      <IntroSection />
+      {children}
+      <FooterBenefits />
+      <Footer />
+    </>
+  );
+}
+
+export default CartLayout;
--- src/components/common/IconButton.tsx
+++ src/components/common/IconButton.tsx
@@ -2,15 +2,16 @@ import { ComponentType, FC, SVGProps } from "react";
 
 type Props = {
   size?: number;
+  color?: string;
   icon: ComponentType<SVGProps<SVGSVGElement>>;
   onClick?: () => void;
 }
 
-const IconButton: FC<Props> = ({ size, icon: Icon, onClick }) => {
+const IconButton: FC<Props> = ({ size, icon: Icon, color, onClick }) => {
   const s = size ?? 24;
 
   return (
-    <button type="button" onClick={onClick} className="hover:cursor-pointer" style={{width: s, height: s}}>
+    <button type="button" onClick={onClick} className="hover:cursor-pointer" style={{width: s, height: s, color}}>
       <Icon />
     </button>
   );
