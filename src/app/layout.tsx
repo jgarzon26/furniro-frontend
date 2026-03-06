9341a60edf6c130835394965763f190bed1123e7
jgarzon26 <janusseangarzon@gmail.com>
2026-03-06 12:33:16 +0800

    separating footer from root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 import { Metadata } from 'next';
 import './globals.css';
 import Header from '../components/header/Header';
-import Footer from '@/components/footer/Footer';
 
 export const metaData: Metadata = {
   title: 'Furniro',
@@ -15,7 +14,6 @@ const RootLayout: FC<LayoutProps<'/'>> = ({children}) => {
       <body>
         <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import Footer from "@/components/footer/Footer";
 import { CategorySection, IntroSection, ProductSection, DisplaySection, ImageGallerySection } from "@/components/home";
 
 const HomePage = () => {
   return (
-    <main>
-      <IntroSection />
-      <CategorySection />
-      <ProductSection />
-      <DisplaySection />
-      <ImageGallerySection />
-    </main>
+    <>
+      <main>
+        <IntroSection />
+        <CategorySection />
+        <ProductSection />
+        <DisplaySection />
+        <ImageGallerySection />
+      </main>
+      <Footer />
+    </>
   );
 }
 
