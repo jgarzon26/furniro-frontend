befa3fb8ee1e5da5cfeba2c3af9140a421ffcb5c
jgarzon26 <janusseangarzon@gmail.com>
2026-03-06 13:20:26 +0800

    reorganize common index

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,8 +1,8 @@
-export { default as IconButton } from './IconButton';
-export { default as Spacer } from './Spacer';
-export { default as SizedBox } from './SizedBox';
-export { default as TextButton } from './TextButton';
-export { default as Placeholder } from './Placeholder';
-export { default as CardItem } from './CardItem';
-export { default as Badge } from './Badge';export { default as IntroSection } from "./IntroSection";
+export { default as Badge } from "./Badge";
+export { default as CardItem } from "./CardItem";
+export { default as IconButton } from "./IconButton";
 export { default as IntroSection } from "./IntroSection";
+export { default as Placeholder } from "./Placeholder";
+export { default as Spacer } from "./Spacer";
+export { default as SizedBox } from "./SizedBox";
+export { default as TextButton } from "./TextButton";
