66f01ba536e48c02b3ed338cc93704327c9561fd
jgarzon26 <janusseangarzon@gmail.com>
2026-03-07 16:38:05 +0800

    remove spacing

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -6,4 +6,3 @@ export { default as Guarantee } from './guarantee.png';
 export { default as Share } from './share.png';
 export { default as Shipping } from './shipping.png';
 export { default as Trophy } from './trophy.png';
-
