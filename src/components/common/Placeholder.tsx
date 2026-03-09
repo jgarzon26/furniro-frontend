6fd1115b45fd93a3b16553b6432e52477e49dfd9
jgarzon26 <janusseangarzon@gmail.com>
2026-03-09 14:44:20 +0800

    added children option for placeholder

--- src/components/common/Placeholder.tsx
+++ src/components/common/Placeholder.tsx
@@ -1,12 +1,12 @@
-import { FC } from "react";
+import { FC, PropsWithChildren } from "react";
 import { twMerge } from "tailwind-merge";
 
 type Props = {
   className?: string;
 }
 
-const Placeholder: FC<Props> = ({className}) => {
-  return <div className={twMerge('h-full w-full bg-gray-300', className)}/>;
+const Placeholder: FC<Props & PropsWithChildren> = ({className, children}) => {
+  return <div className={twMerge('h-full w-full bg-gray-300 flex items-center justify-center', className)}>{children}</div>;
 }
 
 export default Placeholder;
