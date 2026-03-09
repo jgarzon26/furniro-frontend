d9af06a48de901f545c89832a627e41c445230cf
jgarzon26 <janusseangarzon@gmail.com>
2026-03-09 14:45:04 +0800

    added images display section

--- src/components/shop-detail/ProductDetailSection.tsx
+++ src/components/shop-detail/ProductDetailSection.tsx
@@ -4,6 +4,8 @@ import ProductImagesDisplay from "./ProductImagesDisplay";
 const ProductDetailSection: FC = () => {
   return (
     <section className="h-[70dvh] w-full flex flex-row justify-between gap-20">
+      <ProductImagesDisplay />
+      <section className="flex-1 "></section>
     </section>
   );
 }
