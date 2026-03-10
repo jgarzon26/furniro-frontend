b021e1dc93d18bf02064945baa2564040e397be3
jgarzon26 <janusseangarzon@gmail.com>
2026-03-10 17:17:50 +0800

    slight adjustment part 2

--- src/components/shop-detail/ProductDetailSection.tsx
+++ src/components/shop-detail/ProductDetailSection.tsx
@@ -4,6 +4,7 @@ import { Rating } from "@/components/common";
 import SizeSelection from "./SizeSelection";
 import ColorSelection from "./ColorSelection";
 import ShopActions from "./ShopActions";
+import ProductMetaDetails from "./ProductMetaDetails";
 
 type Props = {
   slug: string;
@@ -13,12 +14,12 @@ const ProductDetailSection: FC<Props> = ({slug}) => {
   const testids = ['1','2','3','4','5'];
 
   return (
-    <section className="h-[70dvh] w-full flex flex-row justify-between gap-20">
-      <ProductGallery items={testids} className="flex flex-1 flex-row gap-10">
+    <section className="h-[75dvh] w-full flex flex-row justify-between gap-20">
+      <ProductGallery items={testids} className="flex flex-1 flex-row gap-10 h-9/12">
         <ProductGalleryList />
         <ProductGallerySelected />
       </ProductGallery>
-      <section className="flex flex-col flex-1 gap-5">
+      <section className="flex flex-col flex-2 gap-5">
         <article>
           <h2 className="font-normal text-[42px]">Product Title</h2>
           <h4 className="font-medium text-shop-detail-highlight">Rs. 250,000.00</h4>
@@ -27,7 +28,7 @@ const ProductDetailSection: FC<Props> = ({slug}) => {
           <Rating count={4.5}/>
           <p className="text-shop-detail-highlight text-[13px] border-l border-l-shop-detail-divider px-5">5 Customer Review</p>
         </article>
-        <article className="w-2/3">
+        <article className="w-2/3 flex-1">
           <p>Short description</p>
         </article>
         <SizeSelection />
