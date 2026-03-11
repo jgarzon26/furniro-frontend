78f6e4ea2edf1d0630f3b786942451d251b3a693
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 10:51:05 +0800

    init product info section

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,14 +1,15 @@
 
 import { ProductDetailSection } from "@/components/shop-detail";
+import ProductInfoSection from "@/components/shop-detail/ProductInfoSection";
 import { FC } from "react";
 
 const ShopDetailPage: FC<PageProps<'/shop/[slug]'>> = async ({ params }) => {
   const { slug } = await params;
 
   return (
-    <main className="px-10 py-5">
     <main className="*:p-10 flex flex-col gap-20">
       <ProductDetailSection slug={slug}/>
+      <ProductInfoSection />
     </main>
   );
 }
---
+++ src/components/shop-detail/ProductInfoSection.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import PanelView from "./PanelView";
+import PanelNav from "./PanelNav";
+
+const ProductInfoSection: FC = () => {
+  return (
+  );
+}
+
+export default ProductInfoSection;
