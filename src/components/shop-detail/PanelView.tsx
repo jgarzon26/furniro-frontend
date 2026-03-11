2709ae729ea8c4932266ce23500141c6f34f6555
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 10:50:24 +0800

    added panel context provider

---
+++ src/components/shop-detail/PanelView.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { createContext, FC, ReactNode, use, useState } from "react";
+
+type Props = {
+  className?: string;
+  renderItems: ReactNode[];
+  navItem: ReactNode;
+  navLocation?: 'top' | 'bottom';
+}
+
+type PanelState = {
+  index: number;
+  length: number;
+}
+
+type PanelAction = {
+  setIndex: (index: number) => void;
+}
+
+type PanelContextType = PanelState & PanelAction;
+
+const initialData: PanelState = {
+  index: 0,
+  length: 0,
+}
+
+const PanelContext = createContext<PanelContextType | undefined>(undefined);
+
+export const usePanelContext = () => {
+  const context = use(PanelContext);
+
+  if(!context) {
+    throw new Error('usePanelContext should be a descendent of PanelView');
+  }
+
+  return context;
+}
+
+const PanelView: FC<Props> = ({className, renderItems, navItem, navLocation = 'top'}) => {
+  const [panel, setPanel] = useState<PanelState>(initialData);
+
+  const context: PanelContextType = {
+    ...panel,
+    length: renderItems.length,
+    setIndex: (index) => {setPanel(prev => ({...prev, index: Math.min(Math.max(index, 0), length)}))},
+  }
+
+  return (
+    <PanelContext value={context}>
+      <section className={className}>
+        {navLocation === 'top' && navItem}
+        {renderItems}
+        {navLocation === 'bottom' && navItem}
+      </section>
+    </PanelContext>
+  );
+}
+
+export default PanelView;
