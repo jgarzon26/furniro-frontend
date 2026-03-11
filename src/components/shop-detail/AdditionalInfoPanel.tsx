b9acb0a06e3c43d2979512e01e3cc1247bb98c2c
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 11:36:36 +0800

    init other panels

---
+++ src/components/shop-detail/AdditionalInfoPanel.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const AdditonalInfoPanel: FC = () => {
+  return (
+    <article></article>
+  );
+}
+
+export default AdditonalInfoPanel;
---
+++ src/components/shop-detail/Reviews.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+const Reviews: FC = () => {
+  return (
+    <article></article>
+  );
+}
+
+export default Reviews;
