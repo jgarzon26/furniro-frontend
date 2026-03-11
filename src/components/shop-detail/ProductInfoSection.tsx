5631227ffe226e061b7f9b45764a2411d84bedf2
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 11:38:10 +0800

    added other panels to render

--- src/components/shop-detail/ProductInfoSection.tsx
+++ src/components/shop-detail/ProductInfoSection.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import PanelView from "./PanelView";
 import PanelNav from "./PanelNav";
 import DescriptionPanel from "./DescriptionPanel";
+import AdditionalInfoPanel from "./AdditionalInfoPanel";
+import Reviews from "./Reviews";
 
 const ProductInfoSection: FC = () => {
   return (
@@ -9,6 +11,8 @@ const ProductInfoSection: FC = () => {
       navItem={<PanelNav />} 
       renderItems={[<DescriptionPanel key={'desc'}/>]} 
       className="h-[70dvh] border-t border-shop-detail-divider"
+        <AdditionalInfoPanel  key={'info'}/>,
+        <Reviews key={'reviews'}/>,
     />
   );
 }
