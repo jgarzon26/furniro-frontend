5f031aba25ab33e51b126fdea9f58800cd776b3d
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 11:37:44 +0800

    adding panel switching

--- src/components/shop-detail/PanelNav.tsx
+++ src/components/shop-detail/PanelNav.tsx
@@ -1,13 +1,18 @@
 'use client';
 
 import { FC } from "react";
+import { usePanelContext } from "./PanelView";
 
 const PanelNav: FC = () => {
+  const { index, setIndex } = usePanelContext();
+  const navStyle = "font-normal text-shop-detail-highlight";
+  const navStyleActive = 'font-medium text-foreground';
+
   return (
-    <nav className="flex flex-row items-center justify-center gap-15 [&_h4]:font-medium [&_h4]:text-shop-detail-highlight [&_h4]:hover:cursor-pointer">
-      <h4>Description</h4>
-      <h4>Additional Information</h4>
-      <h4>Reviews [5]</h4>
+    <nav className="flex flex-row items-center justify-center gap-15 [&_h4]:hover:cursor-pointer">
+      <h4 onClick={() => setIndex(0)} className={`${index === 0 ? navStyleActive : navStyle}`}>Description</h4>
+      <h4 onClick={() => setIndex(1)} className={`${index === 1 ? navStyleActive : navStyle}`}>Additional Information</h4>
+      <h4 onClick={() => setIndex(2)} className={`${index === 2 ? navStyleActive : navStyle}`}>Reviews [5]</h4>
     </nav>
   );
 }
