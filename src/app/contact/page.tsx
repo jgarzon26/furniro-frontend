fd9dbbc356b9559a476e6de3e1370611e4c294fb
jgarzon26 <janusseangarzon@gmail.com>
2026-03-11 16:00:51 +0800

    added heading

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,14 @@ import { FC } from "react";
 
 const ContactPage: FC<PageProps<'/contact'>> = () => {
   return (
-    <main></main>
+    <main className="p-20 flex flex-col gap-10">
+      <section className="flex flex-col items-center justify-center gap-1">
+        <h3 className="text-[36px] font-semibold">Get In Touch With Us</h3>
+        <p className="text-contact-highlight w-1/3 text-center">
+          For More Information About Our Product & Services. Please Feel Free To Drop Us An Email. Our Staff Always Be There To Help You Out. Do Not Hesitate!
+        </p>
+      </section>
+    </main>
   );
 }
 
