8a7d281fd5a370856ec88fc966b7f4b01526d4c3
jgarzon26 <janusseangarzon@gmail.com>
2026-03-12 12:16:03 +0800

    added link styling to nav

--- src/components/header/HeaderNav.tsx
+++ src/components/header/HeaderNav.tsx
@@ -1,18 +1,22 @@
 import { FC } from "react";
 import navs from "@/common/navs";
 import Link from "next/link";
+import { headers } from "next/headers";
 
 type Props = {
   className?: string;
 };
 
-const HeaderNav: FC<Props> = ({className}) => {
+const HeaderNav: FC<Props> = async ({className}) => {
+  const headersList = await headers();
+  const pathname = headersList.get('x-current-path');
+
   return (
     <nav className={`list-none *:px-10 ${className}`}>
       {
         navs.map(({link, title}) => (
           <li key={link}>
-            <Link href={link}>{title}</Link>
+            <Link href={link} className={`${pathname === link ? 'font-semibold' : ''} hover:underline`}>{title}</Link>
           </li>
         ))
       }
