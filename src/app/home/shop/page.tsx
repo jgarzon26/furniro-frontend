6aaa2590cad426ec9da0a49f164d866299a6b51a
jgarzon26 <janusseangarzon@gmail.com>
2026-03-24 15:09:34 +0800

    convert shop section to accept children

--- src/app/home/shop/page.tsx
+++ src/app/home/shop/page.tsx
@@ -1,11 +1,16 @@
 import { IntroSection } from "@/components/common";
 import { ShopSection } from "@/components/shop";
+import { OptionsTabSection, PaginationSection, ShopGridSection } from "@/components/shop";
 
 const ShopPage = () => {
   return (
     <main className="flex flex-col">
       <IntroSection />
-      <ShopSection />
+      <ShopSection>
+        <OptionsTabSection />
+        <ShopGridSection />
+        <PaginationSection />
+      </ShopSection>
     </main>
   );
 }
--- src/components/shop/ShopSection.tsx
+++ src/components/shop/ShopSection.tsx
@@ -4,8 +4,7 @@ import { useShopOptionsStore } from "@/hooks/useShopOptionsStore";
 import { GET_PRODUCTS_BY_PAGE } from "@/lib/documents/products";
 import { GetProductsByPageQuery, GetProductsByPageQueryVariables } from "@/types/generated/graphql";
 import { QueryRef, useBackgroundQuery } from "@apollo/client/react";
-import { createContext, FC, Suspense, useContext } from "react";
-import { OptionsTabSection, PaginationSection, ShopGridSection } from ".";
+import { createContext, FC, PropsWithChildren, Suspense, useContext } from "react";
 import HomeLoading from "@/app/home/loading";
 
 type ProductQueryRef = QueryRef<GetProductsByPageQuery, GetProductsByPageQueryVariables, "complete" | "streaming">;
@@ -22,7 +21,7 @@ export const useShopQueryRefContext = () => {
   return context;
 }
 
-const ShopSection: FC = () => {
+const ShopSection: FC<PropsWithChildren> = ({ children }) => {
   const { page, show, short } = useShopOptionsStore(state => state);
   const [queryRef] = useBackgroundQuery(GET_PRODUCTS_BY_PAGE, {
     variables: {
@@ -38,9 +37,7 @@ const ShopSection: FC = () => {
   return (
     <ShopQueryRefContext value={ctx}>
       <Suspense fallback={<HomeLoading />}>
-        <OptionsTabSection />
-        <ShopGridSection />
-        <PaginationSection />
+        {children}
       </Suspense>
     </ShopQueryRefContext>
   );
