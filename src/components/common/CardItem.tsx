448530e2cc14c924ede83d604e6f031572e17cb9
jgarzon26 <janusseangarzon@gmail.com>
2026-03-24 17:42:34 +0800

    added link to shop slug

--- src/components/common/CardItem.tsx
+++ src/components/common/CardItem.tsx
@@ -4,16 +4,17 @@ import TextButton from "./TextButton";
 import { Compare, Favorite, Share } from "@/assets/icons";
 import { FC } from "react";
 import { ProductShortFragment } from "@/types/generated/graphql";
+import Link from "next/link";
 
 type Props = {
   product: ProductShortFragment;
 }
 
-const icons: {title: string, icon: StaticImageData}[] = [
+const icons: { title: string, icon: StaticImageData }[] = [
   {
     icon: Share,
     title: 'Share',
-  }, 
+  },
   {
     icon: Compare,
     title: 'Compare',
@@ -29,40 +30,42 @@ const CardItem: FC<Props> = ({ product }) => {
 
   return (
     <li className="relative h-125 w-75 group *:duration-300">
-      <article className="absolute w-full h-full bg-card-background flex flex-col">
-        <div className="flex-1 relative">
-          <Image src={images[0] ?? ''} alt={title} fill/>
-          <Badge />
-        </div>
-        <div className="flex flex-col gap-1 p-5">
-          <h4>{title}</h4>
-          <p className="text-card-desc">{subtitle}</p>
-          <div className="flex flex-row justify-between items-center">
-            <h5 className={`text-[20px] ${discountedPrice && 'text-decoration-line'}`}>${discountedPrice ? discountedPrice : price}</h5>
-            <p className="text-card-desc line-through">{discountedPrice && `$${price}`}</p>
+      <Link href={`/home/shop/${slug}`}>
+        <article className="absolute w-full h-full bg-card-background flex flex-col">
+          <div className="flex-1 relative">
+            <Image src={images[0] ?? ''} alt={title} fill />
+            <Badge />
           </div>
-        </div>
-      </article>
-      <article className="absolute w-full h-full opacity-0 group-hover:opacity-100">
-        <div className="absolute w-full h-full bg-card-hover-background/72"/>
-        <div className="absolute w-full h-full z-10 flex flex-col items-center justify-center gap-5">
-          <TextButton className="bg-button-secondary text-button-secondary-foreground py-3">
-            <p className="font-bold">Add To Cart</p>
-          </TextButton>
-          <ul className="flex flex-row justify-between gap-5">
-            {
-              icons.map(({icon, title}, index) => (
-                <li key={index}>
-                  <TextButton className="p-0 bg-transparent flex flex-row gap-1">
-                    <Image src={icon} alt={title}/>
-                    <TextButton.Text className="font-semibold">{title}</TextButton.Text>
-                  </TextButton>
-                </li>
-              ))
-            }
-          </ul>
-        </div>
-      </article>
+          <div className="flex flex-col gap-1 p-5">
+            <h4>{title}</h4>
+            <p className="text-card-desc">{subtitle}</p>
+            <div className="flex flex-row justify-between items-center">
+              <h5 className={`text-[20px] ${discountedPrice && 'text-decoration-line'}`}>${discountedPrice ? discountedPrice : price}</h5>
+              <p className="text-card-desc line-through">{discountedPrice && `$${price}`}</p>
+            </div>
+          </div>
+        </article>
+        <article className="absolute w-full h-full opacity-0 group-hover:opacity-100">
+          <div className="absolute w-full h-full bg-card-hover-background/72" />
+          <div className="absolute w-full h-full z-10 flex flex-col items-center justify-center gap-5">
+            <TextButton className="bg-button-secondary text-button-secondary-foreground py-3">
+              <p className="font-bold">Add To Cart</p>
+            </TextButton>
+            <ul className="flex flex-row justify-between gap-5">
+              {
+                icons.map(({ icon, title }, index) => (
+                  <li key={index}>
+                    <TextButton className="p-0 bg-transparent flex flex-row gap-1">
+                      <Image src={icon} alt={title} />
+                      <TextButton.Text className="font-semibold">{title}</TextButton.Text>
+                    </TextButton>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </article>
+      </Link>
     </li>
   );
 }
