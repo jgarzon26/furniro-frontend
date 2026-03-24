ca1950329dc96011ddeeafe9d48c2e74d1898cc6
jgarzon26 <janusseangarzon@gmail.com>
2026-03-24 15:00:21 +0800

    integrate apollo client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,22 @@ import { FC } from 'react';
 import { Metadata } from 'next';
 import './globals.css';
 import Header from '../components/header/Header';
+import ApolloWrapper from '@/components/ApolloWrapper';
 
 export const metaData: Metadata = {
   title: 'Furniro',
   description: 'Interior Furniture Ecommerce Site',
 };
 
-const RootLayout: FC<LayoutProps<'/'>> = ({children}) => {
+const RootLayout: FC<LayoutProps<'/'>> = ({ children }) => {
   return (
     <html lang='en'>
       <body>
-        <div id='modal'/>
-        <Header />
-        {children}
+        <ApolloWrapper>
+          <div id='modal' />
+          <Header />
+          {children}
+        </ApolloWrapper>
       </body>
     </html>
   );
