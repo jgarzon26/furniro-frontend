389e71365e5b9805619a2b925a7c33a4ec95e3a6
jgarzon26 <janusseangarzon@gmail.com>
2026-03-27 16:36:42 +0800

    update carousel list to be more versatile

--- src/components/common/Carousel/CarouselList.tsx
+++ src/components/common/Carousel/CarouselList.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
+import { useCarouselContext } from "./Carousel";
 
 type Props<T> = {
   data: T[];
@@ -8,9 +11,17 @@ type Props<T> = {
 }
 
 const CarouselList = <T,>({ data, renderItems, className }: Props<T>) => {
+  const { itemsPerView, index } = useCarouselContext();
+  const startIndex = index;
+  const endIndex = Math.min(startIndex + itemsPerView, data.length)
+  const visibleData = data.slice(startIndex, endIndex);
+
   return (
-    <ul className={twMerge('w-full flex flex-row justify-stretch items-center gap-5 overflow-x-auto overflow-y-hidden px-10 py-5', className)}>
-      {data.map(item => renderItems?.(item))}
+    <ul 
+      className={twMerge('w-full flex flex-row justify-stretch items-center gap-5 overflow-hidden px-10 py-5', className)}
+      style={{ width: `${(itemsPerView * 100)}%` }}
+    >
+      {visibleData.map(item => renderItems?.(item))}
     </ul>
   );
 }
