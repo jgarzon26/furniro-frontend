fe0697d5bcb6a322975669e52e567ba0397f7b15
jgarzon26 <janusseangarzon@gmail.com>
2026-04-08 15:29:10 +0800

    added token server actions

---
+++ src/lib/api/token.ts
@@ -0,0 +1,23 @@
+'use server';
+
+import { cookies } from "next/headers";
+
+const TOKEN_KEY = 'token';
+
+//TODO: inform gql to add expire time
+
+export const setAuthBearerToken = async (token: string) => {
+  const cookieStore = await cookies();
+  cookieStore.set(TOKEN_KEY, token, { secure: true });
+}
+
+export const getAuthBearerToken = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get(TOKEN_KEY);
+  return `Bearer ${token?.value}`;
+}
+
+export const deleteAuthBearerToken = async () => {
+  const cookieStore = await cookies();
+  cookieStore.delete(TOKEN_KEY);
+}
