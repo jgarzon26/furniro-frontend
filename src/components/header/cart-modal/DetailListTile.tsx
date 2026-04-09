1303d5574fdc6ef109692a3ac76e7fba98bd7ac0
jgarzon26 <janusseangarzon@gmail.com>
2026-04-09 19:26:36 +0800

    cart api implementation to view

--- src/components/header/cart-modal/DetailListTile.tsx
+++ src/components/header/cart-modal/DetailListTile.tsx
@@ -1,18 +1,27 @@
 import { IconButton, Placeholder } from "@/components/common";
 import { Close } from "@/components/common/icons";
+import { CartItemShortFragment } from "@/types/generated/graphql";
+import { FC } from "react";
 
-const DetailListTile = () => {
+type Props = {
+  item: CartItemShortFragment;
+}
+
+const DetailListTile: FC<Props> = ({ item }) => {
+  const { product, quantity } = item;
+  const { title, price } = product;
+
   return (
     <li className="flex flex-row items-center gap-3">
       <div className="flex-1">
         <Placeholder className="aspect-square rounded-[10px]" />
       </div>
       <div className="flex flex-3 flex-col ml-3 gap-1">
-        <h5 className="font-normal">Product Title</h5>
+        <h5 className="font-normal">{title}</h5>
         <div className="flex flex-row items-center justify-between gap-5">
-          <span>1</span>
+          <span>{quantity}</span>
           <span className="text-[12px]">X</span>
-          <span className="text-[12px] text-primary flex-1">Rs. 250,000.00</span>
+          <span className="text-[12px] text-primary flex-1">$ {price}</span>
           <span />
         </div>
       </div>
